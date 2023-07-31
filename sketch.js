39b8b337f3644eedaeb57e83097d30a1aa022987
George2983 <141033216+George2983@users.noreply.github.com>
2023-07-31 17:52:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var ball;
+
+function setup() {
+  createCanvas(600,600);
+  ball = createSprite(300,300,30,30);
+
+}
+
+function draw() 
+{
+   background("red");
+
+ 
+ 
+
+  drawSprites();
+}
+
+
+
+
